a5c1118dca72268e72d8bec2b0e107aee98209f8
Nircko <nir.goldfriend@mail.huji.ac.il>
2024-08-21 14:56:48 +0300

    add src data

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# memories_sequences
-A simulation of memories_sequences
+# Learning Temporal Sequences
+
+## Table of Contents
+1. [Introduction](#introduction)
+2. [Temporal Sequences of Patterns](#temporal-sequences-of-patterns)
+3. [Biological Eveidence](#biological-evidence)
+4. [Simulation](#simulation)
+
+
+
+## Introduction
+
+This lecture discusses the concept of learning and controlling temporal sequences, focusing on the stabilization of sequence states and the interplay between different patterns over time.
+
+## Temporal Sequences of Patterns
+
+Temporal sequences of patterns refer to a series of states or patterns that evolve over time in a dynamic system. The study focuses on understanding how these patterns are stabilized and how transitions between them can be controlled or predicted.
+
+## Biological Eveidence
+
+This theoretical and mathematical formulation of memory sequences has been observed by biologists and physiologists in a marine sea slug species known as Tritonia diomedea. This organism is particularly fascinating due to its simple yet effective neural network. Despite possessing a relatively primitive nervous system compared to more complex animals, Tritonia diomedea demonstrates sophisticated escape behaviors when threatened.
+
+The neural network of Tritonia diomedea is composed of a small number of neurons, each playing a critical role in the slug's survival. When the sea slug detects a predator, it initiates a series of rapid, undulating movements to swim away. These movements are governed by rhythmic motor patterns, where each pattern corresponds to a specific deterministic state within a relatively small number of neurons. These states collectively form the escape swimming behavior.
+
+
+## Simulation
+In our simulation, the neural system is modeled in a way that mirrors the state transitions of Tritonia diomedea's neurons. As illustrated in the spin state figure, this simulation can represent both the neural-muscle patterns of Tritonia diomedea and numerical digits. The simulation demonstrates that, given a trigger (such as the presence of a predator), a deterministic temporal sequence of memory is initiated, leading to independent transitions between different states or digits. This provides compelling evidence of how memory sequences operate, whether in primitive creatures like Tritonia diomedea or in more complex systems such as machines or humans.
+
+In the simulation A deterministic relationship is explored between the weight function, transient time \( t_0 \), and \( \lambda \).
+
+## Assumptions
+
+Several assumptions are made in the analysis:
+
+- Before the sequence starts (for \( t < 0 \)), the state of the spins is random.
+- A transition to the next pattern occurs at time \( t_0 \).
+- Initially, \( T = 0 \).
+
+## Illustration of State Transitions
+
+The state \( S_j(t) \) and its corresponding averaged state are illustrated with different weight functions:
+
+### All right reserved:
+This repository contains material related to the lecture on "Learning Temporal Sequences" presented by Nir Goldfriend on 2017 in the Hebrew University of Jerusalem based on the 1986 work of H. Sompolinsky and I. Kanter in the context of controlling the stabilization of sequences' states. This research laid the groundwork for the theoretical foundations of machine learning in its early days, contributing to some of the most advanced technologies developed in later years.
+
+
---
+++ src/memories_sequences.m
@@ -0,0 +1,125 @@
+%% Parameters of the System Characteristics and Dynamics
+N = 500;            % Number of neurons
+q = 4;              % Number of asymmetric patterns
+delta = 1;          % Difference between q and p
+p = q + delta;      % Number of total patterns
+duration = 100;     % Simulation time duration
+lambda = 1.5;       % Scaling factor for asymmetric synapses
+tau = 8;            % Time decay constant
+
+%% Generating Random Patterns of the Neural Network
+xi_s = 2 .* ceil(rand(N, delta) - 0.5) - 1;   % Symmetric patterns
+xi_as = 2 .* ceil(rand(N, q) - 0.5) - 1;      % Asymmetric patterns
+xi = [xi_as xi_s];                            % Combined pattern matrix
+
+%% Synapses Construction
+J = xi * xi';                 % Initial synaptic matrix
+J1 = (J - diag(diag(J))) / N; % Symmetric synaptic matrix, normalized
+J2 = circshift(xi_as, -1, 2) * xi_as'; % Asymmetric synaptic matrix
+J2 = (J2 - diag(diag(J2))) * (lambda / N); % Asymmetric matrix, normalized
+
+%% Initialization
+S = zeros(N, duration);                        % Initialize state matrix
+S(:, 1) = xi_as(:, 1);                         % Initial state
+Sbar = 2 .* ceil(rand(N, 1) - 0.5) - 1;        % Averaged initial state
+h1 = J1 * xi_as(:, 1);                         % Initial input from symmetric synapses
+h2 = J2 * Sbar;                                % Initial input from asymmetric synapses
+h = h1 + h2;                                   % Total initial input
+
+%% Simulation Loop
+for t = 1:duration
+    S(:, t + 1) = sign(h);  % Update state based on total input
+    w = tau^-1 * exp(-tau^-1 .* (t - (0:t)));  % Weight function
+    Sbar = sum(w .* S(:, 1:t + 1), 2);  % Averaged state over time
+    h1 = J1 * S(:, t + 1);   % Update input from symmetric synapses
+    h2 = J2 * Sbar;          % Update input from asymmetric synapses
+    h = h1 + h2;             % Update total input
+end
+
+%% Plotting Results
+m = S(:, 1:duration)' * xi_as; % Overlap between state and patterns
+plot(m, 'LineWidth', 1.2);
+legend(strsplit(num2str(1:p)));
+xlabel('Time');
+xlim([0 duration]);
+ylim([0 6 * N / 5]);
+ylabel('m^{\nu}', 'FontSize', 24);
+title('The Overlap of the \xi^{\nu} Pattern', 'FontName', 'Impact', 'FontSize', 18);
+
+%% Loading and Processing Images
+xi = zeros(50 * 50, 5);
+figure_load = figure('Name', 'Counting Numbers', 'NumberTitle', 'off');
+% create a path of png files contating digits images
+srcFiles = dir('');
+
+for i = 1:length(srcFiles)
+    filename = fullfile(srcFiles(i).folder, srcFiles(i).name);
+    I = im2double(im2bw(imread(filename)));
+    subplot(2, 3, i);
+    imagesc(I);
+    colormap gray;
+    drawnow;
+    xi(:, i) = I(:);
+end
+
+%% Further Synapse Calculations and Simulation
+xi_as = 2 .* xi - 1; % Binarize the matrix
+N = size(xi, 1);
+Q = (N)^-1 * (xi_as' * xi_as);
+p = 8;
+lambda = 2.5;
+duration = 100;
+tau = 8;
+xi_s = 2 .* ceil(rand(N, p) - 0.5) - 1;
+xi = [xi_as xi_s];
+Y2 = inv((N)^-1 * (xi' * xi));
+Y = inv(Q);
+J1 = (xi * Y2 * xi' - diag(diag(xi * Y2 * xi'))) / N;
+J2 = circshift(xi_as, -1, 2) * Y * xi_as';
+J2 = (J2 - diag(diag(J2))) * (lambda / N);
+
+S = zeros(N, duration); 
+S(:, 1) = xi_as(:, 1); 
+Sbar = 2 .* ceil(rand(N, 1) - 0.5) - 1; 
+h1 = J1 * xi_as(:, 1); 
+h2 = J2 * Sbar; 
+h = h1 + h2;
+
+for t = 1:duration
+    S(:, t + 1) = sign(h);
+    w = tau^-1 * exp(-tau^-1 .* (t - (0:t)));
+    Sbar = sum(w .* S(:, 1:t + 1), 2);
+    h1 = J1 * S(:, t + 1);
+    h2 = J2 * Sbar;
+    h = h1 + h2;
+end
+
+%% Image Processing and Simulation Visualization
+Rimage = zeros(50, 50, duration + 1);
+Sbw = floor(S / 2 + 1);
+
+for t = 1:duration
+    Rimage(:, :, t) = reshape(Sbw(:, t), 50, 50);
+end
+
+Preview = figure('Name', 'Live View', 'NumberTitle', 'off', ...
+    'Position', [432 162 708 536]);
+
+vidObj = VideoWriter('simulation3.avi');
+vidObj.Quality = 100;
+vidObj.FrameRate = 2;
+open(vidObj);
+
+for t = 1:duration
+    subplot(1, 2, 1);
+    barh(Rimage(:, :, t)(:));
+    title(['State of the System at t = ', num2str(t)]);
+    subplot(1, 2, 2);
+    imagesc(Rimage(:, :, t));
+    colormap gray;
+    drawnow;
+    writeVideo(vidObj, getframe(gcf));
+end
+
+close(vidObj);
+winopen('simulation3.avi');
